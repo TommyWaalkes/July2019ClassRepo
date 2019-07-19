1d86eaa2221699a7eb088ca682c1ab4be38a90b7
Tommy Waalkes <Tommy@grandcircus.co>
2019-07-18 20:21:40 -0400

    Added Mock Assessment

---
+++ MockAssessment1/MockAssessment1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace MockAssessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsVowel('a'));
+            Console.WriteLine(CountVowels("aaaaaardvark"));
+            Console.WriteLine(TooManyVowels("aaa aaa a"));
+        }
+
+        public static bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            string vs = "aeiou";
+
+            if (vs.Contains(c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int CountVowels(string input)
+        {
+            char[] letters = input.ToArray();
+
+            int total = 0;
+
+            foreach (char letter in letters)
+            {
+                if(IsVowel(letter) == true)
+                {
+                    total++;
+                }
+            }
+
+            return total;
+        }
+
+        public static string TooManyVowels(string input)
+        {
+            int totalVowels = CountVowels(input);
+            if(totalVowels <= 4)
+            {
+                return "few vowels";
+            }
+            else if(totalVowels >=5 && totalVowels <= 7)
+            {
+                return "right amount of vowels";
+            }
+            else
+            {
+                return "too many vowels";
+            }
+        }
+    }
+}
