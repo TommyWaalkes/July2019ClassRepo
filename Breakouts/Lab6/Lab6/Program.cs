c8853f98f43c22f2ef6c6cea06a6ff2a0d5c94b2
Tommy Waalkes <Tommy@grandcircus.co>
2019-08-07 22:00:01 -0400

    Added Lab 6

---
+++ Breakouts/Lab6/Lab6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            List<string> names = new List<string>{ "Jeff", "James", "Jane", "Jake" };
+            for (int i = 0; i < 100; i++)
+            {
+                int indexRandom = r.Next(0, names.Count);
+                string name = names[indexRandom];
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Please input the number of sides for the dice you will roll");
+            string input = Console.ReadLine();
+            int sides = int.Parse(input);
+            while (true) {
+                int roll1 = RollDie(sides, r);
+                int roll2 = RollDie(sides, r);
+
+                Console.WriteLine(roll1);
+                Console.WriteLine(roll2);
+
+                Console.WriteLine("Would you like to roll again? y/n");
+                string answer = Console.ReadLine();
+                if(answer == "n")
+                {
+                    break;
+                }
+            }
+        }
+
+        public static int RollDie(int sides, Random r)
+        {
+            int output = r.Next(1, sides + 1);
+            return output;
+        }
+    }
+}
