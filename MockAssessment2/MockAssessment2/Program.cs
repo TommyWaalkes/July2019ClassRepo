96058e02d5ac95a35fb1f07c50edd4639be001b5
Tommy Waalkes <Tommy@grandcircus.co>
2019-08-08 21:28:08 -0400

    Mock assessment 2

---
+++ MockAssessment2/MockAssessment2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MockAssessment2
+{
+    class Program
+    {
+        public static List<string> Tasks = new List<string>();
+        static void Main(string[] args)
+        {
+            AddTask("Laundry");
+            AddTask("Mowing");
+            foreach (string task in Tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+
+            FinishTask("Mowing");
+            Console.WriteLine();
+            Console.WriteLine("List after removing:");
+            foreach(string task in Tasks)
+            {
+                Console.WriteLine(task);
+            }
+        }
+
+        public static bool FinishTask(string taskToRemove)
+        {
+            if (Tasks.Contains(taskToRemove))
+            {
+                Tasks.Remove(taskToRemove);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public static void AddTask(string task)
+        {
+            Tasks.Add(task);
+        }
+
+        public static string GetTask(int index)
+        {
+            try
+            {
+                return Tasks[index];
+            }
+            catch(Exception e)
+            {
+                return "Error";
+            }
+        }
+
+
+    }
+}
