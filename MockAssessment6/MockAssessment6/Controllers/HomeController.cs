1fa5d71b6771f9d5b49ec5f3019af6809ef0b2cf
Tommy Waalkes <Tommy@grandcircus.co>
2019-11-04 17:34:32 -0500

    Added Mock Assessment 6

---
+++ MockAssessment6/MockAssessment6/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MockAssessment6.Models;
+
+namespace MockAssessment6.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Employees()
+        {
+            List<Employee> employees = db.Employee.ToList();
+
+            return View(employees);
+        }
+
+        public IActionResult RetirementInfo(int Employee)
+        {
+            Employee e = db.Employee.Find(Employee);
+
+            if(e.Age > 60)
+            {
+                ViewBag.canRetire = true;
+            }
+            else
+            {
+                ViewBag.canRetire = false;
+            }
+            
+            ViewBag.benefits = decimal.Parse(0.6.ToString()) * e.Salary;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MockAssessment6/MockAssessment6/Models/EmployeeContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MockAssessment6.Models
+{
+    public partial class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+        {
+        }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=LAPTOP-FPF5GBKU\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.FirstName).HasMaxLength(40);
+
+                entity.Property(e => e.Salary).HasColumnType("money");
+            });
+        }
+    }
+}
