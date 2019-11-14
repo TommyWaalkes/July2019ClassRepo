a3145e568da564b3b28f1325129c397ec8faeaf0
Tommy Waalkes <Tommy@grandcircus.co>
2019-11-13 21:44:32 -0500

    Added Mock Assessment 7

---
+++ MockAssessment7/MockAssessment7/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MockAssessment7.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MockAssessment7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(int Id)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp($"https://grandcircusco.github.io/demo-apis/donuts/{Id}.json");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream s = response.GetResponseStream();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string apiText = rd.ReadToEnd();
+
+            JToken t = JToken.Parse(apiText);
+
+            Donut d = new Donut(t);
+            ViewBag.Id = Id;
+
+
+            return View(d);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MockAssessment7/MockAssessment7/Models/Donut.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockAssessment7.Models
+{
+    public class Donut
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string PhotoUrl { get; set; }
+        public List<string> Extras { get; set; }
+
+        public Donut()
+        {
+
+        }
+
+        public Donut(JToken t)
+        {
+            this.Name = t["name"].ToString();
+            this.Id = int.Parse(t["id"].ToString());
+            this.Calories = int.Parse(t["calories"].ToString());
+            this.Extras = new List<string>();
+            if (t["extras"] != null)
+            {
+                foreach (JToken extra in t["extras"].ToList())
+                {
+                    this.Extras.Add(extra.ToString());
+                }
+            }
+
+            if (t["photo"] != null)
+            {
+                this.PhotoUrl = t["photo"].ToString();
+            }
+            
+        }
+    }
+}
